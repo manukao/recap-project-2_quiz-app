c4947953302f16b88c58717e84c7cac0aba9041a
Unam <manuelsko@live.de>
2023-02-23 10:33:10 +0100

    add toggle to bookmark and answer button

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector(".bookmark");
+bookmarkButton.addEventListener("click", toggleBookmark);
+
+function toggleBookmark() {
+  bookmarkButton.classList.toggle("bookmark--active");
+  console.log("toggled");
+}
+
+const answerButton = document.querySelector(".card__button-answer");
+answerButton.addEventListener("click", toggleAnswer);
+
+function toggleAnswer() {
+  const answer = document.querySelector(".card__answer");
+  answer.classList.toggle("hidden");
+
+  if (answer.classList.contains("hidden")) {
+    answerButton.textContent = "Show Answer";
+  } else {
+    answerButton.textContent = "Hide Answer";
+  }
+}
